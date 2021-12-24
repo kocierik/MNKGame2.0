78411050488fa21bafde81dfef400ced15b01740
kocierik <kocierik@gmail.com>
2021-12-24 16:06:10 +0100

    feat: add some transposition func

--- mnkgame/S.java
+++ mnkgame/S.java
@@ -1,5 +1,6 @@
 package mnkgame;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Random;
 import java.security.*;
@@ -15,7 +16,18 @@ public class S implements MNKPlayer {
 	private MNKBoard B;
 	private long currentHash;
 	private int M,N,K;
-	
+	private long key = 0;
+
+
+	  // Transposition table keys are hashed using the Zobrist technique
+  // Cache entry structure: [marked, lastCell, searchDepth, type, value]
+  // Marked and lastCell are used to avoid collisions. Type can be one of:
+  // EXACT_VALUE => value is the exact evaluation of the board
+  // UPPER_BOUND => value is the upper bound
+  // LOWER_BOUND => value is the lower bound
+  private static final int EXACT_VALUE = 0, UPPER_BOUND = 1, LOWER_BOUND = -1;
+  private HashMap<Long, int[]> cache = new HashMap<>();
+
 	/**
 	 * Default empty constructor
 	 */
@@ -55,6 +67,41 @@ public class S implements MNKPlayer {
 public int heuristic(){
 	throw new Error("Heuristic not implemented yet");
 }
+
+private long getZobrist(int i, int j){
+	return key ^ zobristTable[i][j][B.currentPlayer()];
+}
+
+public long zobrist() {
+	return key;
+}
+
+public int marked() {
+	return B.MC.size();
+}
+
+private int[] transposition(final int searchDepth) {
+	MNKCell[] c = B.getMarkedCells();
+	return transposition(zobrist(), marked() , c[c.length - 1].i * Math.min(M,N) + c[c.length - 1].j, searchDepth);
+}
+
+  // Returns a cache entry for the current board. If the current board is already
+  // in the transposition table the entry contains the actual data, otherwhise
+  // its fields 2,3 are dummy. A non-cached board can be therefore identified
+  // by entry[3] == 2. Cost: O(1)
+  private int[] transposition(final long hash, final int marked, final int lastCell, final int searchDepth) {
+    if (cache.containsKey(hash)) {
+      int[] cached = cache.get(hash);
+      // Make sure the board has the same number of marked symbols and the last
+      // cell marked matches. This is done to avoid false positives in the cache
+      if (cached[0] == marked && cached[1] == lastCell && cached[2] >= searchDepth
+      // useless
+          && cached[3] != 2)
+        return cached;
+    }
+
+    return new int[] { marked, lastCell, searchDepth, 2, Integer.MIN_VALUE };
+  }
 
 
 private MNKGameState markCell(int i, int j) {
@@ -120,7 +167,6 @@ public double alphabetaPruning(boolean isMaximizing, int depth, double alpha, do
 	// Fulcro dell'applicativo che esegue le funzioni citate sopra
 	public MNKCell selectCell(MNKCell[] FC, MNKCell[] MC){
 		start = System.currentTimeMillis();	
-
 		if(MC.length > 0) {
 			MNKCell d = MC[MC.length-1]; 
 			B.markCell(d.i,d.j);         
@@ -138,14 +184,14 @@ public double alphabetaPruning(boolean isMaximizing, int depth, double alpha, do
 
 		//----------------------------------------------------------------
 		
+		int beta = 100, alpha = -100;
 		// Esegue alphaBetaPruning e tutte le euristiche applicate
 		for(MNKCell d : FC) {
 			if ((System.currentTimeMillis()-start)/1000.0 > TIMEOUT*(99.0/100.0)) {
 				break;
 			} else {	
 				B.markCell(d.i, d.j);	
-				if(B.M <= 6) score = alphabetaPruning(true,6,-1000,1000);
-				else if(B.M <= 10) score = alphabetaPruning(true,4,-1000,1000);
+				score = alphabetaPruning(true,6,alpha,beta);
 				B.unmarkCell();
 				if (score > bestScore){
 					bestScore = score;
@@ -153,6 +199,7 @@ public double alphabetaPruning(boolean isMaximizing, int depth, double alpha, do
 				} 
 			}
 		} 
+		System.out.println("found best cell with value (" + alpha + "," + beta + ") at " + bestMoves);
 		B.markCell(bestMoves.i, bestMoves.j);
 		return bestMoves;
 	}
