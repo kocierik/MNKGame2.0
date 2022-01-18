d857b41c6ea2b59724050961871efe99fa21550c
Paolo Ceroni <paoloceroni01@gmail.com>
2022-01-18 21:14:46 +0100

    step 1 heur

--- src/mnkgame/S.java
+++ src/mnkgame/S.java
@@ -79,21 +79,87 @@ public class S implements MNKPlayer {
 		else if(state == myWin) return M*N*1_000;
     else return -M*N*1_000;
   }
+
+public int longSeriesBonus(int n, int consecutive, int marked){
+  if(n>=K){
+    if(consecutive == K-1) return 10_000_000; //10M
+    if(consecutive == K-2) return 100_000;    //100k
+    if(consecutive == K-3) return 1_000;      //1k
+    else return (marked/n) * 1_000;
+  }
+  return 0;
 }
 
+//sequenza finale, aggiungere marked longSeriesBonus
 public int depthCell(int i, int j, int dir_i, int dir_j, int maxIter){
-  int res = 0;
+  int value = 0;
+  //last_player è 1 o 2
+  int lastPlayer = 0;
+  int prev = -1;
+  int marked = 0, series = 0, maxSeries = 0;
+  int c1series = 0, c2series = 0, lastFreeSeries = 0;
   for(int z=0;z<maxIter;z++){
-    if(B.cellState(i+z*dir_i,j+z*dir_j) == MNKCellState.P1)
-      res++;
+    if(B.cellState(i+z*dir_i,j+z*dir_j) == MNKCellState.P1){
+      if(lastPlayer==2){
+        value -= longSeriesBonus(c2series, maxSeries,marked);
+        maxSeries = 0;
+        c2series = 0;
+        c1series = 1 + lastFreeSeries;
+        marked = 1;
+      }
+      else{
+        if(prev==1) series++;
+        else{
+          if(series>maxSeries) maxSeries = series;
+          series = 1;
+        }
+        marked++;
+        c1series++;
+      }
+      lastFreeSeries = 0;
+      lastPlayer = 1;
+      prev = 1;
     }
-  return res;
+    else if(B.cellState(i+z*dir_i,j+z*dir_j) == MNKCellState.P2){
+      if(lastPlayer==1){
+        value += longSeriesBonus(c1series, maxSeries, marked);
+        maxSeries = 0;
+        c1series = 0;
+        c2series = 1 + lastFreeSeries;
+        marked = 1;
+      }
+      else{
+        if(prev==2) series++;
+        else {
+          if(series>maxSeries) maxSeries = series;
+          series = 1;
+        }
+        marked++;
+        c2series++;
+      }
+      lastFreeSeries = 0;
+      lastPlayer = 2;
+      prev = 2;
+    }
+    else if(B.cellState(i+z*dir_i,j+z*dir_j) == MNKCellState.FREE){
+      if(lastPlayer==1){
+        c1series++;
+      }
+      if(lastPlayer==2){
+        c2series++;
+      }
+      if(prev!=1 && prev!=2){
+        lastFreeSeries++;
+      }
+      else lastFreeSeries = 1;
+    }
+  }
+  return value;
 }
-
 
 public int heuristic() {
-  int i,j,value;
-  //fow
+  int i,j,value = 0;
+  //row
   for (i = 0; i < M; i++) {
     j = 0;
     value += depthCell(i,j,0,1,N);
